d802bebd21178fd80457480882781e46d8fde465
evaldoe397 <evaldoe397@gmail.com>
2025-07-08 19:04:26 -0300

    Projeto inicial Ferramenta Function no interface funcional

---
+++ src/entities/Product.java
@@ -0,0 +1,54 @@
+package entities;
+
+import java.text.DecimalFormat;
+
+public class Product{
+	private String name;
+	private Double price;
+	
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >=100.0;
+	}
+	
+	public boolean nonStaticProductPredicate() {
+		return price >= 100.0;
+	}
+	
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+	
+	public void nonStaticPriceUpdate() {
+		price = price * 1.1;
+	}
+	
+
+	@Override
+	public String toString() {
+		DecimalFormat df = new DecimalFormat("#.##");
+		return "Product " + name + ", " + df.format(price);
+	}
+
+		
+}
